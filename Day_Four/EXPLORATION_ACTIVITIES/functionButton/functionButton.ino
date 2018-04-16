c354fdd04ff40f30da544d05d68f0dc6ee704690
Liza Stark <lizastark@gmail.com>
2018-04-16 17:40:55 -0400

    update pins

--- Day_Four/EXPLORATION_ACTIVITIES/functionButton/functionButton.ino
+++ Day_Four/EXPLORATION_ACTIVITIES/functionButton/functionButton.ino
@@ -2,28 +2,41 @@
   FUNCTION WITH A BUTTON
 
   Girls Who Code
-  Day 4: Button Power Challenge
+  Day 4: Using functions with a button
   Example 4
 
-  This example uses the LilyPad ProtoSnap Development Board to turn an LED on and off 
+  This example uses the LilyPad ProtoSnap Development Board to turn LEDs on and off
   using a push button.
 
 */
 
 // constants won't change. They're used here to
 // set pin numbers:
-const int buttonPin = 5;     // the number of the push button pin
+const int buttonPin = A5;     // the number of the push button pin
+const int ledOne = 5;
+const int ledTwo = 6;
+const int ledThree = A2;
+const int ledFour = A4;
+const int ledFive = A3;
+
 
 // variables will change:
 int buttonState = 0;         // variable for reading the push button status
 
 void setup() {
-    //Start the serial monitor
+  //Start the serial monitor
   Serial.begin(9600);
-  
+
+  // tell Arduino these are outputs
+  pinMode(ledOne, OUTPUT);
+  pinMode(ledTwo, OUTPUT);
+  pinMode(ledThree, OUTPUT);
+  pinMode(ledFour, OUTPUT);
+  pinMode(ledFive, OUTPUT);
+
   // initialize the push button pin as an input:
   pinMode(buttonPin, INPUT_PULLUP);
-  
+
 }
 
 void loop() {
@@ -32,21 +45,41 @@ void loop() {
 
   // check if the push button is pressed.
   if (buttonState == LOW) {
-    
-    // turn LED on:
-    sayHi();
-    
+
+    turnOnEvenLEDs();
+
+  } else {
+
+    turnOnOddLEDs();
+
   }
-  
+
 }
 
-// create your function
-void sayHi() {
+// turn on all the even LEDs in bottom row
+void turnOnEvenLEDs() {
 
-  // tasks the function will perform when called
-  Serial.println("Hey girl!");
-  
-  //add a delay
-  delay(300);
-  
+  digitalWrite(ledOne, LOW);
+  digitalWrite(ledTwo, HIGH);
+  digitalWrite(ledThree, LOW);
+  digitalWrite(ledFour, HIGH);
+  digitalWrite(ledFive, LOW);
+
+  //for debugging
+  Serial.println("Evens are on!");
+
+}
+
+// turn on all the odd LEDs in bottom row
+void turnOnOddLEDs() {
+
+  digitalWrite(ledOne, HIGH);
+  digitalWrite(ledTwo, LOW);
+  digitalWrite(ledThree, HIGH);
+  digitalWrite(ledFour, LOW);
+  digitalWrite(ledFive, HIGH);
+
+  //for debugging
+  Serial.println("Odds are on!");
+
 }
